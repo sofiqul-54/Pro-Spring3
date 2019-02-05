27799c07d0378d99779b16cc88ab2307e341dc2e
sofiqul-54 <sofiqul54@gmail.com>
2019-02-05 12:30:40 +0600

    spring 2nd c

---
+++ Work/autowiring/src/main/java/com/sofiqul54/AutowiringApplication.java
@@ -0,0 +1,14 @@
+package com.sofiqul54;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutowiringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutowiringApplication.class, args);
+    }
+
+}
+
---
+++ Work/beanlifecyclefive/src/main/java/com/sofiqul54/MyAwareService.java
@@ -0,0 +1,66 @@
+package com.sofiqul54;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanClassLoaderAware;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.*;
+import org.springframework.context.annotation.ImportAware;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.core.type.AnnotationMetadata;
+
+import java.util.Arrays;
+
+public class MyAwareService implements ApplicationContextAware, ApplicationEventPublisherAware, BeanClassLoaderAware,
+        BeanFactoryAware, BeanNameAware, EnvironmentAware, ImportAware, ResourceLoaderAware {
+
+    @Override
+    public void setBeanClassLoader(ClassLoader classLoader) {
+        System.out.println("setBeanClassLoader called");
+        System.out.println("setBeanClassLoader:: Class Loder Name"+ classLoader.getClass().getName());
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println("setBeanFactory called");
+        System.out.println("setBeanFactory:: employee bean singleton"+ beanFactory.isSingleton("employee"));
+    }
+
+    @Override
+    public void setBeanName(String beanName) {
+        System.out.println("setBeanName called");
+        System.out.println("setBeanName:: Bean Name defined in context=" + beanName);
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext ctx) throws BeansException {
+        System.out.println("setApplicationContext called");
+        System.out.println("setApplicationContext:: bean dafination  name"+ Arrays.toString(ctx.getBeanDefinitionNames()));
+    }
+
+    @Override
+    public void setApplicationEventPublisher(ApplicationEventPublisher applicationEventPublisher) {
+        System.out.println("setApplicationEventPublisher called");
+
+    }
+
+    @Override
+    public void setEnvironment(Environment environment) {
+        System.out.println("setEnvironment");
+    }
+
+    @Override
+    public void setResourceLoader(ResourceLoader resourceLoader) {
+        System.out.println("setResourceLoader called");
+        Resource resource = resourceLoader.getResource("classpath:spring.xml");
+        System.out.println("setResourceLoader:: Resource File Name=" + resource.getFilename());
+    }
+
+    @Override
+    public void setImportMetadata(AnnotationMetadata annotationMetadata) {
+        System.out.println("setImportMetadata");
+    }
+}

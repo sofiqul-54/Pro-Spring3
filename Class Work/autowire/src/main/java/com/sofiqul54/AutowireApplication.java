1dfe8cdb199bc817c7cba0cab7ae8a97cabbf0e8
sofiqul-54 <sofiqul54@gmail.com>
2019-02-06 09:08:16 +0600

    spring 3rd c

---
+++ Class Work/autowire/src/main/java/com/sofiqul54/AutowireApplication.java
@@ -0,0 +1,14 @@
+package com.sofiqul54;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutowireApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutowireApplication.class, args);
+    }
+
+}
+

6e3a8fdbeaacc0f5c949056460189805c04ab604
sofiqul-54 <sofiqul54@gmail.com>
2019-02-17 09:10:40 +0600

    spring 10th c

---
+++ Resources/Sir/Code/securibasic/src/main/java/com/coderbd/securibasic/SecuribasicApplication.java
@@ -0,0 +1,14 @@
+package com.coderbd.securibasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuribasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecuribasicApplication.class, args);
+    }
+
+}
+

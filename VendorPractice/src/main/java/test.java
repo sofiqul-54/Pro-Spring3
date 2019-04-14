52153e07879dcd2214380e31f76a88b2e6efef3d
sofiqul-54 <sofiqul54@gmail.com>
2019-04-14 16:22:12 +0600

    vendor

---
+++ VendorPractice/src/main/java/test.java
@@ -0,0 +1,9 @@
+public class test {
+    public static void main(String[] args) {
+        int ii = 0;
+        int jj = 7;
+        for(ii=0; ii <jj-1;  ii = ii+2){
+            System.out.println(ii + " ");
+        }
+    }
+}

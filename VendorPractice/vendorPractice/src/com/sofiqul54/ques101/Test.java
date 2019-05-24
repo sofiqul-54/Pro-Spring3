09f29ea6e77e4b1032a57a52afedd729235d50bd
sofiqul-54 <sofiqul54@gmail.com>
2019-05-24 23:45:41 +0600

    vendor  practice

---
+++ VendorPractice/vendorPractice/src/com/sofiqul54/ques101/Test.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sofiqul54.ques101;
+
+/**
+ *
+ * @author sofiqul54
+ */
+public class Test {
+    static int count = 0;
+    int i = 0;
+    
+    public void changeCount(){
+    while(i<5){
+    i++;
+    count++;
+    }
+    } 
+    public static void main(String[] args) {
+        Test check1 = new Test();
+        Test check2 = new Test();
+        check1.changeCount();
+        check2.changeCount();
+        System.out.println(check1.count+" : " + check2.count);
+        
+    }
+    
+}
